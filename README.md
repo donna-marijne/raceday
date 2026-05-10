b3f3df06f866c16fed9366728111f6f3db349d4e
donna <donnamarijne+gh@gmail.com>
2026-05-10 21:53:41 +0100

    fix missing sector duration bug

--- README.md
+++ README.md
@@ -40,6 +40,4 @@ A terminal tool for replaying Formula 1 races!
 
 ## Bugs to fix
 
-- Cars may have `null` as `duration_sector_*` meaning that we calculate the position incorrectly
-  - Case: session 11245, car 43, lap 4; calculated as position 1, expected ~2
-  - Solution: if one sector is missing, use the `lap_duration` to derive it
+None so far!
--- openf1/timing_events.py
+++ openf1/timing_events.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+import log
 from sector import Sector
 from timing_event import TimingEvent
 
@@ -13,20 +14,23 @@ def timing_events_from_api_laps(laps, cars):
         duration_sector_1 = lap["duration_sector_1"]
         duration_sector_2 = lap["duration_sector_2"]
         duration_sector_3 = lap["duration_sector_3"]
+        lap_duration = lap["lap_duration"]
         driver_number = lap["driver_number"]
         lap_number = lap["lap_number"]
 
         car = cars[driver_number]
 
         if date_start is None:
-            # DNS(?)
+            log.debug(f"date_start is None: {lap}")
             continue
 
         last_end = datetime.fromisoformat(date_start)
-        for i, duration in enumerate(
-            [duration_sector_1, duration_sector_2, duration_sector_3]
-        ):
+        sector_durations = _fixup_sector_durations(
+            lap_duration, duration_sector_1, duration_sector_2, duration_sector_3
+        )
+        for i, duration in enumerate(sector_durations):
             if duration == None:
+                log.debug(f"duration_sector_{i} is None: {lap}")
                 continue
 
             sector_end = last_end + timedelta(seconds=duration)
@@ -55,3 +59,32 @@ def timing_events_from_api_laps(laps, cars):
         timing_event.car_position = len(positions_by_sector[sector])
 
     return timing_events
+
+
+def _fixup_sector_durations(
+    lap_duration, duration_sector_1, duration_sector_2, duration_sector_3
+):
+    durations = [duration_sector_1, duration_sector_2, duration_sector_3]
+
+    if lap_duration is None:
+        # car retired, nothing to do
+        return durations
+
+    durations_total = 0.0
+    missing_sector = None
+    for i, duration in enumerate(durations):
+        if duration is not None:
+            durations_total += duration
+            continue
+
+        if missing_sector is not None:
+            raise Exception(f"Lap completed with more than one missing sector")
+
+        missing_sector = i
+
+    if missing_sector is not None:
+        # here we have one missing sector time and a lap_duration
+        # so we can infill the difference
+        durations[missing_sector] = lap_duration - durations_total
+
+    return durations
